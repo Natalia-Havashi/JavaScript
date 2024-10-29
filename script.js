1bc83c23c9bf24a2b29b5d5f4dfe492d75239fe5
Natalia-Havashi <natali.gavashi@gmail.com>
2024-10-29 18:13:12 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// import * as Variables from './VariablesandDataTypes';
+
+const name = "Natalia";
+console.log(name);
+
+const age = 25;
+console.log(typeof age);
+
+const number = 10 + "20";
+console.log(number);
+console.log(typeof number);
