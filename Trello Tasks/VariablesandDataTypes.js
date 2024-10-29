84d9bdd33c6db77ae562447cfe61898cbc54d306
Natalia-Havashi <natali.gavashi@gmail.com>
2024-10-29 18:24:47 +0200

    Variables and Data Types

--- Trello Tasks/VariablesandDataTypes.js
+++ Trello Tasks/VariablesandDataTypes.js
@@ -1,3 +1,11 @@
+const name = "Natalia";
+console.log(name);
 
+let age = 25;
+age = String(age)
+console.log(typeof age);
 
-// export * from "./VariablesandDataTypes";
+const number = 10 + "20";
+console.log(number);
+console.log(typeof number);
+
--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// import * as Variables from './VariablesandDataTypes';
-
-const name = "Natalia";
-console.log(name);
-
-const age = 25;
-console.log(typeof age);
-
-const number = 10 + "20";
-console.log(number);
-console.log(typeof number);
+import * as Variables from "./Trello Tasks/VariablesandDataTypes.js";
