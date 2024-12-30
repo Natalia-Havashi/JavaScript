9f1ff861a05b97f688a9351a35b3a1957d3d5e64
Natalia-Havashi <natali.gavashi@gmail.com>
2024-12-30 23:14:36 +0200

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 import * as Variables from "./Trello Tasks/VariablesandDataTypes.js";
 import * as Array from "./Trello Tasks/array.js";
-import * as Object from './Trello Tasks/object.js'
-import * as Function from './Trello Tasks/function.js'
+import * as Object from "./Trello Tasks/object.js";
+import * as Function from "./Trello Tasks/function.js";
+import * as Promises from "./Trello Tasks/promises.js";
+
+import * as AsyncAwait from "./Trello Tasks/asyncAwait.js";
+import * as Context from "./Trello Tasks/contextThis.js";
+import * as EventHandl from "./Trello Tasks/eventHandling.js";
+import * as FetchApi from "./Trello Tasks/fetchApi.js";
+import * as LocalStorage from "./Trello Tasks/localStorage.js";
+import * as Patterns from "./Trello Tasks/patterns.js";
+import * as Prototypes from "./Trello Tasks/prototypes.js";
